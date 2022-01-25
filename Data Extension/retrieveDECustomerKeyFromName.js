5117be5781d20118803833a12d61226374e4b649
Matheus Leto <matheus.leto@bexpert.lan.corp>
2022-01-25 14:55:12 -0300

    receive api as argument to avoid missing problems

---
+++ Data Extension/retrieveDECustomerKeyFromName.js
@@ -0,0 +1,13 @@
+function retrieveDECustomerKeyFromName(api, dataExtensionName){
+  var simpleFilter = {
+    Property: 'Name',
+    SimpleOperator: 'equals',
+    Value: dataExtensionName
+  }
+  var result = api.retrieve("DataExtension", ["CustomerKey"], simpleFilter);
+  if(result.Status != 'OK' || result.Results.length < 1){
+    return null
+  }else{
+    return result.Results[0].CustomerKey;
+  }
+}
