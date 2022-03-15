9053a4d8fb271316f89d9a8c8433fcf415d71db1
Leto <matheus.b.leto@accenture.com>
2022-03-15 18:36:29 -0300

    deleteRecordsWithPipe

--- Data Extension/deleteRecords.js
+++ Data Extension/deleteRecords.js
@@ -1,39 +1,47 @@
 // <script runat="server">
+
 Platform.Load("Core", "1.1.5");
 var api = new Script.Util.WSProxy();
 
-var customerKeySourceToRemove = 'QueryStudioResults at 2021-10-28T0047320000';
-var customerKeyToDelete = 'E9FD1328-56C0-426C-8316-7FBEB5465B34';
+var sourceDENameWithRowToDelete = 'Remover Contatos Quarentena'; 
+var tagetDENameToDeleteRows = 'Tb_Base_Quarentena';
+var logDEName = 'resultados delete_2';
+var logDECK = retrieveDECustomerKeyFromName(api, logDEName)
 
 var depara = [
   {
     source: 'CPF_CNPJ',
-    destiny: 'CPF'
+    destiny: 'CPF_CNPJ'
   }
 ];
 
 try{
 
-  var deSourceToExclude = DataExtensionRecordsOperator({api: api, dataExtensionCustomerKey: customerKeySourceToRemove});
-  var sourceDataToExclude = deSourceToExclude.getRecords({ headers: [ depara[0].source ]});
+var deSourceToExclude = DataExtensionRecordsOperator({api: api, dataExtensionName: sourceDENameWithRowToDelete});
 
-  // Write('\nsourceDataToExclude\n');
-  // Write(Stringify(sourceDataToExclude) + '\n');
+function myDeleteFunction(records){
+  // Write('records' + '\n');
+  // Write(Stringify(records) + '\n\n');
 
-  var formatedField = matchFields(sourceDataToExclude, depara);
-
-  // Write('\nformatedField\n');
-  // Write(Stringify(formatedField) + '\n');
-
-  var deleteBatch = convertArrayOfObjectsToDeleteBatch(formatedField, customerKeyToDelete, [ depara[0].destiny ]);
+  // Write('depara' + '\n');
+  // Write(Stringify(depara) + '\n\n');
+  var formatedField = matchFields(records, depara);
   
-  // Write('\ndeleteBatch\n');
-  // Write(Stringify(deleteBatch) + '\n');
-
+  var deleteBatch = convertArrayOfObjectsToDeleteBatch(formatedField, retrieveDECustomerKeyFromName(api, tagetDENameToDeleteRows), [ depara[0].destiny ]);
+  
   var resultDeleteBatch = api.deleteBatch("DataExtensionObject", deleteBatch);
+  
+  
+  insertRecordInDE(api, {RESULTADO: 'Apagados: ' + resultDeleteBatch.Results.length}, logDECK);
+
 
   Write('\nresultDeleteBatch\n');
   Write(Stringify(resultDeleteBatch.Results.length) + '\n');
+
+}
+var sourceDataToExclude = deSourceToExclude.pipeExecuteInAllRecords(null, myDeleteFunction, 2000);
+
+Write('concluido' + '\n');
 
 }catch(e){
   Write('Error');
@@ -46,11 +54,20 @@ function DataExtensionRecordsOperator(configuration){
   
   if(!configuration) Platform.Function.RaiseError('An configuration Object is required');
   if(!configuration.api) Platform.Function.RaiseError('Api attribute is required');
-  if(!configuration.dataExtensionCustomerKey) Platform.Function.RaiseError('dataExtensionCustomerKey attribute is required');
-  if(typeof configuration.dataExtensionCustomerKey != 'string') Platform.Function.RaiseError('dataExtensionCustomerKey must be a String');
-  
+  if(!configuration.dataExtensionCustomerKey && !configuration.dataExtensionName) Platform.Function.RaiseError('dataExtensionCustomerKey or dataExtensionName attribute is required');
+  if(configuration.dataExtensionCustomerKey && typeof configuration.dataExtensionCustomerKey != 'string') Platform.Function.RaiseError('dataExtensionCustomerKey must be a String');
   api = configuration.api;
-  customerKey = configuration.dataExtensionCustomerKey;
+  
+  if(configuration.dataExtensionCustomerKey == null || configuration.dataExtensionCustomerKey == ''){
+    var simpleFilter = {
+      Property: 'Name',
+      SimpleOperator: 'equals',
+      Value: configuration.dataExtensionName
+    }
+    customerKey = api.retrieve("DataExtension", ["CustomerKey"], simpleFilter).Results[0].CustomerKey;
+  }else{
+    customerKey = configuration.dataExtensionCustomerKey;
+  }
 
   function getRecords(options){
     // options Object can have filter or headers
@@ -110,6 +127,65 @@ function DataExtensionRecordsOperator(configuration){
     
   }
 
+  function pipeExecuteInAllRecords(options, cb, batchSize){
+    // options Object can have filter or headers
+    if(!!options && typeof options != 'object') Platform.Function.RaiseError('Options must be an object or omitted');
+    var batchSizeActual = batchSize == null || batchSize == undefined ? 2500 : batchSize;
+    batchSizeActual = batchSizeActual > 2500 ? 2500 : batchSizeActual;
+    if(batchSizeActual == 0) Platform.Function.RaiseError('batchSize cannot be 0');
+        
+    var filter;
+    headers = retrieveFieldNames(customerKey, api);
+
+    if(!!options && options.filter) filter = options.filter;
+
+    var config = {
+      customerKey: customerKey,
+      cols: headers
+    }
+  
+    var records = [],
+    moreData = true,
+    reqID = data = null;
+
+    while (moreData) {
+      moreData = false;
+      if (reqID == null) {
+        if(filter == undefined || filter == null){
+          data = api.retrieve("DataExtensionObject[" + config.customerKey + "]", config.cols);
+        }else{
+          data = api.retrieve("DataExtensionObject[" + config.customerKey + "]", config.cols, filter);
+        }
+        if(data.Status.substring(0, 5) == 'Error'){
+          Platform.Function.RaiseError('Something went wrong: ' + data.Status);
+        }
+      } else {
+        data = api.getNextBatch("DataExtensionObject[" + config.customerKey + "]", reqID);
+      }
+
+      if (data != null) {
+        moreData = data.HasMoreRows;
+        reqID = data.RequestID;
+        var retrievedData = data.Results;
+        while ( retrievedData.length != 0 ) {
+          var currentData = retrievedData.splice(0, batchSizeActual);
+          for (var i = 0; i < currentData.length; i++) {
+            var result_list = currentData[i].Properties;
+            var obj = {};
+            for (k in result_list) {
+              var key = result_list[k].Name;
+              var val = result_list[k].Value
+              if (key.indexOf("_") != 0) obj[key] = val;
+            }
+            records.push(obj);
+          }
+          cb(records); // operate over the first Chunck
+          records = []; // clear records, free up memory
+        }
+      }
+    }
+  }
+
   function retrieveFieldNames(customerKey, api) {
     var filter = {
       Property: "DataExtension.CustomerKey",
@@ -127,7 +203,35 @@ function DataExtensionRecordsOperator(configuration){
   }
 
   return {
-    getRecords: getRecords
+    getRecords: getRecords,
+    pipeExecuteInAllRecords: pipeExecuteInAllRecords
+  }
+}
+
+function insertRecordInDE(api, record, customerKey){
+  if(!api) Platform.Function.RaiseError('Api param is required');
+  var props = [];
+  for(prop in record){
+    props.push( {Name: prop, Value: record[prop] } );
+  }
+  var data = {
+    CustomerKey: customerKey,
+    Properties: props
+  }
+  return api.createItem('DataExtensionObject', data);
+}
+
+function retrieveDECustomerKeyFromName(api, dataExtensionName){
+  var simpleFilter = {
+    Property: 'Name',
+    SimpleOperator: 'equals',
+    Value: dataExtensionName
+  }
+  var result = api.retrieve('DataExtension', ['CustomerKey'], simpleFilter);
+  if(result.Status != 'OK' || result.Results.length < 1){
+    return null
+  }else{
+    return result.Results[0].CustomerKey;
   }
 }
 
